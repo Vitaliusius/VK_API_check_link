3d02cbe53e8d003b791e23601f6a3bfd98cc1436
Vitaliy <vitalianobro@gmail.com>
2026-01-27 14:18:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def check_shorten_link(token, url):
+def is_shorten_link(token, url):
     payload = {
         'access_token': token,
         'v': '5.199',
         'key': urlparse(url).path[1:],
-        'interval': 'week',
-        'intervals_count': 1
     }
     response = requests.post('https://api.vk.ru/method/utils.getLinkStats', params=payload)
     response.raise_for_status()
 
-    return 'error' not in response.json()
+    return response.json()
 
 
 def shorten_link(token, url):
@@ -27,7 +25,7 @@ def shorten_link(token, url):
     response = requests.post('https://api.vk.ru/method/utils.getShortLink', params=payload)
     response.raise_for_status()
 
-    return response.json()
+    return response.json()['response']['short_url']
 
 
 def count_clicks(token, link):
@@ -41,23 +39,23 @@ def count_clicks(token, link):
     response = requests.post('https://api.vk.ru/method/utils.getLinkStats', params=payload)
     response.raise_for_status()
 
-    return response.json()
+    return response.json()['response']['stats'][-1]['views']
 
 
 def main():
     load_dotenv()
     token = os.environ['VK_SERVICE_KEY']
     url = input('Введите ссылку: ')
-
-    if check_shorten_link(token, url):
-        print('Колличество кликов: ', count_clicks(token, urlparse(url).path[1:])['response']['stats'][-1]['views'])
-    else:
-        try:
-            print('Короткая ссылка: ', shorten_link(token, url)['response']['short_url'])
-        except KeyError:
-            print("Ошибка", shorten_link(token, url)['error'])
+    try:
+        if 'error' not in is_shorten_link(token, url):
+            print('Количество кликов: ', count_clicks(token, urlparse(url).path[1:]))
+        else:
+            print('Короткая ссылка: ', shorten_link(token, url))
+    except IndexError:
+        print('Количество кликов: 0')
+    except KeyError:
+        print(is_shorten_link(token, url)['error'])
 
 
 if __name__ == '__main__':
     main()
-
